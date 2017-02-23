ab1f043ce3e639cc395569439c461358d39791a5
ribbon <ribbon@qq.com>
2017-02-23 15:17:34 +0800

    dingwei

--- wx/index.php
+++ wx/index.php
@@ -32,6 +32,9 @@ $signPackage = $jssdk->GetSignPackage();
 			点击扫码
 		</button>
 		<img src="" id="img"/>
+		<button onclick="local()">
+			
+		</button>
 
 		<script type="text/javascript">
 			wx.config({
@@ -82,7 +85,7 @@ $signPackage = $jssdk->GetSignPackage();
 			
 			function test(){
 				wx.chooseImage({
-				    count: 1, // 默认9
+				    count: 1, // 默认9,选择照片的张数
 				    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
 				    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
 				    success: function (res) {
@@ -93,10 +96,26 @@ $signPackage = $jssdk->GetSignPackage();
 			}
 			
 			
-			
-			wx.previewImage({
-    			current: '', // 当前显示图片的http链接
-    			urls: [] // 需要预览的图片http链接列表
+			wx.ready(function(){
+				function local(){
+					wx.getLocation({
+					    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+					    success: function (res) {
+					        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+					        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+					        var speed = res.speed; // 速度，以米/每秒计
+					        var accuracy = res.accuracy; // 位置精度
+					        
+					        wx.startSearchBeacons({
+								ticket:"",  //摇周边的业务ticket, 系统自动添加在摇出来的页面链接后面
+								complete:function(argv){
+									//开启查找完成后的回调函数
+									alert("定位成功!");
+								}
+							});
+					    }
+					});
+				}
 			});
 		</script>
 	</body>
