f6b563272c965b26fb5353923f0a2e9b04fd5a91
ribbon <ribbon@qq.com>
2017-02-27 17:46:01 +0800

    1

--- beequick/home/position.php
+++ beequick/home/position.php
@@ -58,75 +58,75 @@ $signPackage = $jssdk->GetSignPackage();
 </div>
 </body>
 <script type="text/javascript">
-    window.onload = function(){
+    window.onload = (function(){
+
         wx.config({
-                    debug: true, // 开启调试功能，如果为true每进行一次操作都会弹出
-                    appId: '<?php echo $signPackage["appId"];?>', // 必填，公众号的唯一标识(字符串)
-                    timestamp: <?php echo $signPackage["timestamp"];?>, // 必填，生成签名的时间戳(数字)
-                    nonceStr: '<?php echo $signPackage["nonceStr"];?>', // 必填，生成签名的随机串(字符串)
-                    signature: '<?php echo $signPackage["signature"];?>',// 必填，签名，见附录1(字符串)
-                    jsApiList: [
-                        'checkJsApi',
-                        'onMenuShareTimeline',
-                        'onMenuShareAppMessage',
-                        'onMenuShareQQ',
-                        'onMenuShareWeibo',
-                        'onMenuShareQZone',
-                        'hideMenuItems',
-                        'showMenuItems',
-                        'hideAllNonBaseMenuItem',
-                        'showAllNonBaseMenuItem',
-                        'translateVoice',
-                        'startRecord',
-                        'stopRecord',
-                        'onVoiceRecordEnd',
-                        'playVoice',
-                        'onVoicePlayEnd',
-                        'pauseVoice',
-                        'stopVoice',
-                        'uploadVoice',
-                        'downloadVoice',
-                        'chooseImage',
-                        'previewImage',
-                        'uploadImage',
-                        'downloadImage',
-                        'getNetworkType',
-                        'openLocation',
-                        'getLocation',
-                        'hideOptionMenu',
-                        'showOptionMenu',
-                        'closeWindow',
-                        'scanQRCode',
-                        'chooseWXPay',
-                        'openProductSpecificView',
-                        'addCard',
-                        'chooseCard',
-                        'openCard'
-                    ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                debug: true, // 开启调试功能，如果为true每进行一次操作都会弹出
+                appId: '<?php echo $signPackage["appId"];?>', // 必填，公众号的唯一标识(字符串)
+                timestamp: <?php echo $signPackage["timestamp"];?>, // 必填，生成签名的时间戳(数字)
+                nonceStr: '<?php echo $signPackage["nonceStr"];?>', // 必填，生成签名的随机串(字符串)
+                signature: '<?php echo $signPackage["signature"];?>',// 必填，签名，见附录1(字符串)
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'onMenuShareQZone',
+                    'hideMenuItems',
+                    'showMenuItems',
+                    'hideAllNonBaseMenuItem',
+                    'showAllNonBaseMenuItem',
+                    'translateVoice',
+                    'startRecord',
+                    'stopRecord',
+                    'onVoiceRecordEnd',
+                    'playVoice',
+                    'onVoicePlayEnd',
+                    'pauseVoice',
+                    'stopVoice',
+                    'uploadVoice',
+                    'downloadVoice',
+                    'chooseImage',
+                    'previewImage',
+                    'uploadImage',
+                    'downloadImage',
+                    'getNetworkType',
+                    'openLocation',
+                    'getLocation',
+                    'hideOptionMenu',
+                    'showOptionMenu',
+                    'closeWindow',
+                    'scanQRCode',
+                    'chooseWXPay',
+                    'openProductSpecificView',
+                    'addCard',
+                    'chooseCard',
+                    'openCard'
+                ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
+
+            //获取位置
+
+                wx.getLocation({
+                    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                    success: function (res) {
+                        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                        var speed = res.speed; // 速度，以米/每秒计
+                        var accuracy = res.accuracy; // 位置精度
+
+                        wx.openLocation({
+                            latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+                            longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+                            name: '育知同创', // 位置名
+                            address: '七星创意工厂', // 地址详情说明
+                            scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                            infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+                        });
+                    }
                 });
-
-                //获取位置
-                (function(){
-                    wx.getLocation({
-                        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                        success: function (res) {
-                            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                            var speed = res.speed; // 速度，以米/每秒计
-                            var accuracy = res.accuracy; // 位置精度
-
-                            wx.openLocation({
-                                latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-                                longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-                                name: '育知同创', // 位置名
-                                address: '七星创意工厂', // 地址详情说明
-                                scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-                                infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
-                            });
-                        }
-                    });
-                })();
-    }
+        })();
 
     </script>
 <script type="text/javascript" src="../lib/flexible.js"></script>
