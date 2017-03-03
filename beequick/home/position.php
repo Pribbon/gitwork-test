b5dad77d1747cedf8dd7751e8a6fdce2862400ce
ribbon <ribbon@qq.com>
2017-03-03 14:41:43 +0800

    1

--- beequick/home/position.php
+++ beequick/home/position.php
@@ -58,7 +58,6 @@ $signPackage = $jssdk->GetSignPackage();
 </div>
 </body>
 <script type="text/javascript">
-    wx.ready(function(){
         wx.config({
             debug: true, // 开启调试功能，如果为true每进行一次操作都会弹出
             appId: '<?php echo $signPackage["appId"];?>', // 必填，公众号的唯一标识(字符串)
@@ -105,28 +104,29 @@ $signPackage = $jssdk->GetSignPackage();
             ]
         });
 
-        function(){
-            wx.getLocation({
-                type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                success: function (res) {
-                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                    var speed = res.speed; // 速度，以米/每秒计
-                    var accuracy = res.accuracy; // 位置精度
-                    console.log(latitude+longitude);
+	wx.ready(function(){
+        wx.getLocation({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            success: function (res) {
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
+                console.log(latitude+longitude);
 
-                    wx.openLocation({
-                        latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-                        longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-                        name: '育知同创', // 位置名
-                        address: '七星创意工厂', // 地址详情说明
-                        scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-                        infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
-                    });
-                }
-            });
-        }
-    });
+                /*
+                 * 打开地图
+                 wx.openLocation({
+                    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+                    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+                    name: '育知同创', // 位置名
+                    address: '七星创意工厂', // 地址详情说明
+                    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+                });*/
+            }
+        });
+	});
 </script>
 <script type="text/javascript" src="../public/lib/flexible.js"></script>
 </html>
